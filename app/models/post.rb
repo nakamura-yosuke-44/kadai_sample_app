0dcf2fa34138ee3348b4896a578265de01a85aa4
nakamura-yosuke-44 <fdora.fantasy.97@gmail.com>
2023-05-10 11:13:06 +0900

    edit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :body, presence: true, length: {minimum: 5, maximum: 140}
+    validates :title, presence: true
+    has_many :comments, dependent: :destroy
 end
